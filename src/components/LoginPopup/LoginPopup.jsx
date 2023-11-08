8933bd7230dd77b23ffefd1edf508c4222d13ee5
shurikperunov@mail.ru <shurikperunov@mail.ru>
2023-11-08 15:43:02 +0500

    плавное открытие модалки

--- src/components/LoginPopup/LoginPopup.jsx
+++ src/components/LoginPopup/LoginPopup.jsx
@@ -1,18 +1,28 @@
 import styles from './LoginPopup.module.scss';
 import { Dialog } from '@headlessui/react';
+import { motion, AnimatePresence } from 'framer-motion';
 import LoginForm from '../LoginForm';
 
 const LoginPopup = ({ isOpen, onClose, title }) => {
   return (
-    <Dialog open={isOpen} onClose={onClose}>
-      <div className={styles.bg}>
-        <Dialog.Panel className={styles.popup}>
-          <Dialog.Title className={styles.title}>{title}</Dialog.Title>
-          <LoginForm buttonText='Войти' />
-          <button className={styles.close} onClick={onClose}></button>
-        </Dialog.Panel>
-      </div>
-    </Dialog>
+    <AnimatePresence>
+      {isOpen && (
+        <Dialog static as={motion.div} open={isOpen} onClose={onClose}>
+          <motion.div
+            className={styles.bg}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <Dialog.Panel className={styles.popup}>
+              <Dialog.Title className={styles.title}>{title}</Dialog.Title>
+              <LoginForm buttonText="Войти" />
+              <button className={styles.close} onClick={onClose}></button>
+            </Dialog.Panel>
+          </motion.div>
+        </Dialog>
+      )}
+    </AnimatePresence>
   );
 };
 
